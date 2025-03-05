5ad65c3fc58370bd1c721471dd54bc7c77beeeee
sarlms <sarah.elmss@icloud.com>
2025-03-05 09:44:06 +0100

    feat: SwiftUI project reation

---
+++ ahoui-ios/ahoui-ios/ahoui_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahoui_iosApp.swift
+//  ahoui-ios
+//
+//  Created by etud on 03/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ahoui_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
